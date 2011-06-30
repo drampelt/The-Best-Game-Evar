47094e21e6ac4c8029d5bdf96f318fc12a7c92d7
zmm22 <zmm2@live.com>
2011-06-30 17:33:35 -0400

    player1.gif to spaceship1.gif

--- Shooter/src/Shooter.java
+++ Shooter/src/Shooter.java
@@ -34,8 +34,8 @@ public class Shooter extends JFrame{
 		setVisible(true);
 		//make it appear
 		
-		player1 = new Player(10, 150, 20, 90, 10, getImage("images/player1.gif"));
-		player2 = new Player(570, 150, 20, 90, 10, getImage("images/player2.gif"));
+		player1 = new Player(10, 150, 20, 90, 10, getImage("images/spaceship1.gif"));
+		player2 = new Player(570, 150, 20, 90, 10, getImage("images/spaceship2.gif"));
 		
 	}
 	
